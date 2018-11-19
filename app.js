27fe7070d9e021514d52ea1f9100441f67fea2d5
Niels Koster <n.koster@portavita.eu>
2018-11-20 00:51:20 +0100

    Test: response = req

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const
 	http = require('http'),
-	fs = require('fs');
+	fs = require('fs'),
+	util = require('util');
 
 http.createServer(function(req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	fs.createReadStream(__dirname + '/index.html', 'utf8')
-		.pipe(res);
-	// res.end(fs.readFileSync(__dirname + '/index.html', 'utf8')
-	// 	.replace('{Message}', 'apekop!'));
+	// res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	let obj = {
+		firstname: 'Guus',
+		lastname: 'Jansen'
+	}
+	res.end(util.inspect(req));
+	//res.end(JSON.stringify(obj));
 }).listen(4444, '127.0.0.1');
